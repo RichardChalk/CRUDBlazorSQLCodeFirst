c48b13a93452c45ac9841de4eae387611db60cd1
Richard Chalk <barmybrit@gmail.com>
2022-07-21 13:31:36 +0200

    Injected context to Champ services

---
+++ CRUDCodeFirst/Services/ChampionServices.cs
@@ -0,0 +1,15 @@
+﻿using CRUDCodeFirst.Data;
+
+namespace CRUDCodeFirst.Services
+{
+    public class ChampionServices
+    {
+        public ApplicationDbContext _context { get; }
+        public ChampionServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       
+    }
+}
