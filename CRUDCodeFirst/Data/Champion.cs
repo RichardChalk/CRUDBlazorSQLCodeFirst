8d9c24f300c955696a3d1ae1298af3b56b8a6e85
Richard Chalk <barmybrit@gmail.com>
2022-07-21 13:20:01 +0200

    Added Champion data entity

--- CRUDCodeFirst/Data/Champion.cs
+++ CRUDCodeFirst/Data/Champion.cs
@@ -1,6 +1,29 @@
-﻿namespace CRUDCodeFirst.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDCodeFirst.Data
 {
+    public enum ChampionClass
+    {
+        Mage,
+        ADC,
+        Assassin,
+        Tank,
+        Bruiser
+    }
+
+    public enum DamageType
+    {
+        AD,
+        AP,
+        Mixed
+    }
     public class Champion
     {
+        [Key]
+        public int ChampionId { get; set; }
+        public string Name { get; set; }
+        public ChampionClass Class { get; set; }
+        public int StartingAttackDamage { get; set; }
+        public DamageType DamageType { get; set; }
     }
 }
