c0d9668297dc5180c8bc8498f8097b253dc5ab31
Richard Chalk <barmybrit@gmail.com>
2022-07-21 13:22:26 +0200

    Created DbContext

---
+++ CRUDCodeFirst/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDCodeFirst.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Champion> Champions { get; set; }
+    }
+}
