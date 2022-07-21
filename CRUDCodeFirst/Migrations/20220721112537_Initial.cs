60ea6cf43641a5ca37335a2b0cea94e91e2dd49c
Richard Chalk <barmybrit@gmail.com>
2022-07-21 13:28:02 +0200

    Finished Db Migration

---
+++ CRUDCodeFirst/Migrations/20220721112537_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDCodeFirst.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Champions",
+                columns: table => new
+                {
+                    ChampionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Class = table.Column<int>(type: "int", nullable: false),
+                    StartingAttackDamage = table.Column<int>(type: "int", nullable: false),
+                    DamageType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Champions", x => x.ChampionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Champions");
+        }
+    }
+}
--- CRUDCodeFirst/Program.cs
+++ CRUDCodeFirst/Program.cs
@@ -1,13 +1,19 @@
 using CRUDCodeFirst.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+// Injecting my DbContext
+builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 
 var app = builder.Build();
 
